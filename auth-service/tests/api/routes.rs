82b1abc9778773b91c90e16fb7bf00f2c48576e3
Mateusz Płaczek <mateusz.placzek@gmail.com>
2024-05-14 11:31:00 +0200

    Implement endpoints into auth service

--- auth-service/tests/api/routes.rs
+++ auth-service/tests/api/routes.rs
@@ -11,5 +11,47 @@ async fn root_returns_auth_ui() {
     assert_eq!(response.headers().get("content-type").unwrap(), "text/html");
 }
 
-// TODO: Implement tests for all other routes (signup, login, logout, verify-2fa, and verify-token)
-// For now, simply assert that each route returns a 200 HTTP status code.
+#[tokio::test]
+async fn signup_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.post_signup().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn login_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.post_login().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn logout_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.post_logout().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn verify_2fa_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.post_verify_2fa().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn verify_token_returns_200() {
+    let app = TestApp::new().await;
+
+    let response = app.post_verify_token().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
